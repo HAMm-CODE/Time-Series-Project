30207a9f36f2e2a564b682814e5e2fa4474846d5
Hamm-code <sentsdrimma123@gmail.com>
2026-02-16 01:18:36 +0200

    Completed step 1

--- Project.R
+++ Project.R
@@ -2,12 +2,38 @@ library(tseries)
 library(forecast)
 library(ggplot2)
 
-# Load the data
+
 data <- read.csv("Case_study.csv")
 y <- data$V82
 
-# Step 4: Split the data
+
 train_data <- y[1:100]
 test_data <- y[101:200]
 
 plot.ts(train_data, main="Time Series Plot of Yt", ylab="Value", col="blue")
+
+acf(train_data, main="ACF of Yt")
+
+acf(diff(train_data, differences = 1), main="ACF of ∇Yt")
+
+acf(diff(train_data, differences = 2), main="ACF of ∇2Yt")
+
+adf_test <- adf.test(train_data)
+print(adf_test)
+
+adf_test1 <- adf.test(diff(train_data, differences = 1))
+print(adf_test1)
+
+adf_test2 <- adf.test(diff(train_data, differences = 2))
+print(adf_test2)
+
+stationary_series <- diff(train_data, differences = 1)
+par(mfrow=c(1,2))
+acf(stationary_series, main="ACF for q")
+pacf(stationary_series, main="PACF for p")
+
+#step 2
+
+
+
+
